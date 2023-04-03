34d50e1b44e22f851474f1b278c59655ee749749
Matheus Pazinati <matheuspazinati@gmail.com>
2023-04-03 18:29:47 -0300

    Cria uma classe para gerenciar as querys no banco de dados

--- src/server.js
+++ src/server.js
@@ -1,19 +1,19 @@
 import http from 'node:http'
 import { json } from './middlewares/json.js'
-import fs from 'node:fs/promises'
+import { Database } from '../src/database.js'
+
 
 const port = 3333
 
-const databasePath = new URL('../db.json', import.meta.url)
+const database = new Database()
 
 const server = http.createServer( async (req, res) => {
   
   await json(req, res)
 
   if (req.method === "GET") {
-    const tasks = await fs.readFile(databasePath, 'utf-8')
-    res.statusCode = 200
-    return res.end(tasks)
+    const tasks = database.select('tasks')
+    res.end(JSON.stringify(tasks))
   }
 })
 
