488b45e8d6cbf1f87a267b17df99830caf39edcb
Matheus Pazinati <matheuspazinati@gmail.com>
2023-04-05 09:17:38 -0300

    Cria rota para criar uma nova tarefa no banco de dados

--- src/routes.js
+++ src/routes.js
@@ -10,5 +10,25 @@ export const routes = [
       const tasks = database.select('tasks')
       return res.end(JSON.stringify(tasks))
     }
+  },
+  {
+    method: "POST",
+    handler: (req, res) => {
+      const { title, description } = req.body
+      if (title && description) {
+        const task = {
+          id: randomUUID(),
+          title,
+          description,
+          created_at: new Date(),
+          updated_at: new Date(),
+          completed_at: null
+        }
+        database.insert('tasks', task)
+        return res.writeHead(201).end()
+      } else {
+        return res.writeHead(422).end()
+      }
+    }
   }
 ]
